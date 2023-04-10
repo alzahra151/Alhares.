578425cda190ecc6be418546560d0115d7129b31
Alzahraa Desoky <33639931+alzahra151@users.noreply.github.com>
2023-04-10 10:39:13 +0200

    fix navbar

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { LanguageService } from 'src/app/services/language.service';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss']
 })
-export class CarouselComponent {
-
+export class CarouselComponent implements OnInit{
+  lang :String='ar'
+constructor(public translate: TranslateService, public language:LanguageService){ }
+  ngOnInit(): void {
+    
+    this.language.languge.subscribe(()=>{
+      console.log( this.language.languge.value)
+      this.lang=this.language.languge.value
+     })
+  }
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild, Renderer2 } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 // import  AOS from 'aos';
 import * as $ from 'jquery';
@@ -8,13 +8,16 @@ import { LanguageService } from 'src/app/services/language.service';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
+
 export class NavbarComponent implements OnInit {
-  navbarVisible = true;
+//  @ViewChild('nav') nav!:ElementRef;
+ navbarVisible:Boolean = true;
+  lang:String="ar"
   previousScrollPosition = 0;
   currentScrollPosition = 0;
-  constructor( public translate: TranslateService, public language:LanguageService){}
+  constructor( public translate: TranslateService, public language:LanguageService ,public elementref:ElementRef ,public renderer: Renderer2){}
   MenuIsopen = false;
-  lang:String="ar"
+ 
   ngOnInit(): void {
     $(document).ready(function() {
       $('.navbar-toggler').click(function() {
@@ -27,24 +30,30 @@ export class NavbarComponent implements OnInit {
      this.lang=this.language.languge.value
     })
      
- 
+    
    
   }
  
-  // @HostListener('window:scroll', [])
-  // onWindowScroll() {
-  //   this.currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
 
-  //   if (this.currentScrollPosition > this.previousScrollPosition) {
-  //     // Scrolling down
-  //     this.navbarVisible = false;
-  //   } else {
-  //     // Scrolling up
-  //     this.navbarVisible = true;
-  //   }
+    if (this.currentScrollPosition > this.previousScrollPosition) {
+      // Scrolling down
+      this.navbarVisible = false;
+      // this.nav.nativeElement.classList.remove('colorNave')
+      // const nav = this.elementref.nativeElement.querySelector('#nav');
+      // this.renderer.setStyle(nav, 'top', '-100px');
+    } else {
+      // Scrolling up
+      this.navbarVisible = true;
+      // this.nav.nativeElement.classList.add('colorNave')
+      // const nav = this.elementref.nativeElement.querySelector('#nav');
+      // this.renderer.setStyle(nav, 'top', '0px');
+    }
 
-  //   this.previousScrollPosition = this.currentScrollPosition;
-  // }
+    this.previousScrollPosition = this.currentScrollPosition;
+  }
  
 
 openMenu() {
