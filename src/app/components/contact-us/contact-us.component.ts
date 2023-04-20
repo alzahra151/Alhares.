b73cb209508274e6566b4ffa57c12e237cab38f9
Alzahraa Desoky <33639931+alzahra151@users.noreply.github.com>
2023-04-20 21:38:50 +0200

    update

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { LanguageService } from 'src/app/services/language.service';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent {
-
+  lang:String='ar'
+  constructor(public translate: TranslateService, public language:LanguageService){}
+ngOnInit(){
+  this.language.languge.subscribe(()=>{
+    console.log( this.language.languge.value)
+    this.lang=this.language.languge.value
+   })
+}
 }
